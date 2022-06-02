67f8cb4930d7849f8be26b20ad7a5a932b9d7151
AR2615 <72493804+AnoushkaRaizada@users.noreply.github.com>
2022-06-03 03:03:09 +0530

    Add files via upload

---
+++ HOSTEL/hostel.js
@@ -0,0 +1,100 @@
+function imagefun() {
+    var Image_Id = document.getElementById('getImage');
+    if (Image_Id.src.match("hostel/1.jpg")) {
+        Image_Id.src = "hostel/2.jpg";
+        document.querySelector(".images").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image2").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    
+    else if (Image_Id.src.match("hostel/2.jpg")) {
+        Image_Id.src = "hostel/3.jpg";
+        document.querySelector("#image2").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image3").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/3.jpg")) {
+        Image_Id.src = "hostel/4.jpg";
+        document.querySelector("#image3").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image4").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/4.jpg")) {
+        Image_Id.src = "hostel/5.jpg";
+        document.querySelector("#image4").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image5").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/5.jpg")) {
+        Image_Id.src = "hostel/6.jpg";
+        document.querySelector("#image5").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image6").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/6.jpg")) {
+        Image_Id.src = "hostel/7.jpg";
+        document.querySelector("#image6").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image7").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/7.jpg")) {
+        Image_Id.src = "hostel/8.jpg";
+        document.querySelector("#image7").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image8").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/8.jpg")) {
+        Image_Id.src = "hostel/9.jpg";
+        document.querySelector("#image8").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image9").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/9.jpg")) {
+        Image_Id.src = "hostel/10.jpg";
+        document.querySelector("#image9").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image10").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/10.jpg")) {
+        Image_Id.src = "hostel/11.jpg";
+        document.querySelector("#image10").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image11").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/11.jpg")) {
+        Image_Id.src = "hostel/12.jpg";
+        document.querySelector("#image11").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image12").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/12.jpg")) {
+        Image_Id.src = "hostel/13.jpg";
+        document.querySelector("#image12").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image13").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/13.jpg")) {
+        Image_Id.src = "hostel/14.jpg";
+        document.querySelector("#image13").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image14").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/14.jpg")) {
+        Image_Id.src = "hostel/15.jpg";
+        document.querySelector("#image14").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image15").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/15.jpg")) {
+        Image_Id.src = "hostel/16.jpg";
+        document.querySelector("#image15").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image16").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/16.jpg")) {
+        Image_Id.src = "hostel/17.jpg";
+        document.querySelector("#image16").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image17").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/17.jpg")) {
+        Image_Id.src = "hostel/18.jpg";
+        document.querySelector("#image17").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image18").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/18.jpg")) {
+        Image_Id.src = "hostel/19.jpg";
+        document.querySelector("#image18").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image19").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    else if (Image_Id.src.match("hostel/19.jpg")) {
+        Image_Id.src = "hostel/20.jpg";
+        document.querySelector("#image19").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image20").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+
+}     
