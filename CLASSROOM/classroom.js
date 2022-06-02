d43a727f71378d5a580aa7544f6a0eafebb24af1
AR2615 <72493804+AnoushkaRaizada@users.noreply.github.com>
2022-06-03 02:25:56 +0530

    Add files via upload

---
+++ CLASSROOM/classroom.js
@@ -0,0 +1,118 @@
+function imagefun() {
+    var Image_Id = document.getElementById('getImage');
+    if (Image_Id.src.match("classroom/1.jpg")) {
+        Image_Id.src = "classroom/2.jpg";
+        document.querySelector(".images").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image2").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+    }
+    
+    else if (Image_Id.src.match("classroom/2.jpg")) {
+        Image_Id.src = "classroom/3.jpg";
+        document.querySelector("#image2").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image3").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/3.jpg")) {
+        Image_Id.src = "classroom/4.jpg";
+        document.querySelector("#image3").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image4").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/4.jpg")) {
+        Image_Id.src = "classroom/5.jpg";
+        document.querySelector("#image4").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image5").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/5.jpg")) {
+        Image_Id.src = "classroom/6.jpg";
+        document.querySelector("#image5").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image6").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/6.jpg")) {
+        Image_Id.src = "classroom/7.jpg";
+        document.querySelector("#image6").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image7").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/7.jpg")) {
+        Image_Id.src = "classroom/8.jpg";
+        document.querySelector("#image7").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image8").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/8.jpg")) {
+        Image_Id.src = "classroom/9.jpg";
+        document.querySelector("#image8").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image9").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/9.jpg")) {
+        Image_Id.src = "classroom/10.jpg";
+        document.querySelector("#image9").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image10").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/10.jpg")) {
+        Image_Id.src = "classroom/11.jpg";
+        document.querySelector("#image10").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image11").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/11.jpg")) {
+        Image_Id.src = "classroom/12.jpg";
+        document.querySelector("#image11").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image12").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/12.jpg")) {
+        Image_Id.src = "classroom/13.jpg";
+        document.querySelector("#image12").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image13").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/13.jpg")) {
+        Image_Id.src = "classroom/14.jpg";
+        document.querySelector("#image13").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image14").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/14.jpg")) {
+        Image_Id.src = "classroom/15.jpg";
+        document.querySelector("#image14").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image15").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/15.jpg")) {
+        Image_Id.src = "classroom/16.jpg";
+        document.querySelector("#image15").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image16").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/16.jpg")) {
+        Image_Id.src = "classroom/17.jpg";
+        document.querySelector("#image16").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image17").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/17.jpg")) {
+        Image_Id.src = "classroom/18.jpg";
+        document.querySelector("#image17").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image18").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/18.jpg")) {
+        Image_Id.src = "classroom/19.jpg";
+        document.querySelector("#image18").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image19").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    else if (Image_Id.src.match("classroom/19.jpg")) {
+        Image_Id.src = "classroom/20.jpg";
+        document.querySelector("#image19").style = "display: none; animation: fadeOut 2s forwards;"
+        document.querySelector("#image20").style = "display: block; animation: fadeIn 2s forwards; margin-left: 12%;"
+      
+    }
+    
+}     
